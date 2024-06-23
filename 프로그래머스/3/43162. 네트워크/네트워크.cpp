804233aa836465ea2033ec3951bb82e23718c223
inDseok <inseok99@gmail.com>
2024-06-23 21:21:29 +0900

    [level 3] Title: 네트워크, Time: 0.47 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+bool visited[201];
+void dfs(int n,int cur,vector<vector<int>> computers){
+    for(int i=0;i<n;i++){
+        if(computers[cur][i]==1&&visited[i]==false){
+            visited[i]=true;
+            dfs(n,i,computers);
+        }   
+    }
+}
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    for(int i=0;i<n;i++){
+        visited[i]=false;
+    }
+    for(int i=0;i<n;i++){
+        if(visited[i]==false){
+            visited[i]=true;
+            dfs(n,i,computers);
+            answer++;
+        }
+    }
+    return answer;
+}
