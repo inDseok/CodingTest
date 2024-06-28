21345b6c2a77d9f03e6d10752d283d639454b787
inDseok <inseok99@gmail.com>
2024-06-28 15:45:23 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -1,21 +1,17 @@
-#include <string>
 #include <vector>
 
 using namespace std;
 
 vector<int> solution(int brown, int yellow) {
-    vector<int> answer;
-    int sum=brown+yellow;
-    for(int i=3;i<=sum;i++){
-        for(int j=3;j<=i;j++){
-            if(i*j==sum){
-                for(int k=1;k<=j/2+1;k++){
-                    if((i-k)*(j-k)==yellow){
-                        answer.push_back(i);
-                        answer.push_back(j);
-                        return answer;
-                    }
-                }
+    vector <int> answer;
+    int total = brown + yellow;
+
+    for (int height = 3; height <= total / height; ++height) {
+        if (total % height == 0) {
+            int width = total / height;
+            if ((width - 2) * (height - 2) == yellow) {
+                answer.push_back(width);
+                answer.push_back(height);
             }
         }
     }
