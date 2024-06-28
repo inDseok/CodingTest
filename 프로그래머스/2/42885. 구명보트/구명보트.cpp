8049e1dc5fb432d2decb23b24b965a5b526087e8
inDseok <inseok99@gmail.com>
2024-06-28 15:24:51 +0900

    [level 2] Title: 구명보트, Time: 1.41 ms, Memory: 4.62 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    int s=0,e=(int)people.size()-1;
+    sort(people.begin(),people.end());
+    while(s<=e)
+    {
+        if(people[s]+people[e]<=limit){
+            answer++;
+            s++;
+            e--;
+        }
+        else{
+            answer++;
+            e--;
+        }
+    }
+    return answer;
+}
