30b457ef35d9d36ce006f0d29a6c7a188aaa42d2
전인석 <inseok99@gmail.com>
2025-02-01 16:34:36 +0900

    [level 3] Title: 네트워크, Time: 3.06 ms, Memory: 6.44 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,14 +1,15 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
+int arr[201][201];
 bool visited[201];
 void dfs(int n,int cur,vector<vector<int>> computers){
     for(int i=0;i<n;i++){
         if(computers[cur][i]==1&&visited[i]==false){
             visited[i]=true;
             dfs(n,i,computers);
-        }   
+        }
     }
 }
 int solution(int n, vector<vector<int>> computers) {
