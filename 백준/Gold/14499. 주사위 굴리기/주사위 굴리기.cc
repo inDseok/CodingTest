27c0baffe6a4f25e9f37f1f945bc7de09ea058c7
전인석 <inseok99@gmail.com>
2025-04-09 21:07:22 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.cc
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.cc
@@ -1,109 +1,96 @@
 #include<iostream>
-
+#include<algorithm>
+#include<vector>
 using namespace std;
-int n, m;
-int x, y;
-int k;
-int arr[21][21]={-1,};
-int dice[6]={0,};
-//동쪽은 1, 서쪽은 2, 북쪽은 3, 남쪽은 4
-//밑 0, 위 1, 왼 2, 오른 3, 앞 4, 뒤 5
-int main()
-{
-	cin >> n >> m >> x >> y >> k;
-	for (int i = 0; i < n; i++){
+int arr[21][21];
+int dice[6] = { 0,0,0,0,0,0 };//위0 북1 동2 서3 남4 밑5
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m, x, y, k;
+	cin >> n >> m >> x >> y >> k; //x가 행 y가 열
+	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> arr[i][j];
 		}
 	}
-	for (int i = 0; i < k; i++)
-	{
-		int dir;
-		cin >> dir;
-		switch (dir)
-		{
-		case 1:
-		{
-			if (y+1 >= m)
+	vector<int> move(k);
+	for (int i = 0; i < k; i++) {
+		cin >> move[i];
+	}
+	for (int i = 0; i < move.size(); i++) {
+		if (move[i] == 1) { //동
+			if (y+1 >= m) 
 				continue;
-			y ++;
-			int temp;
-			temp = dice[0];
+			y = y + 1;
+			int temp = dice[5];
+			dice[5] = dice[2];
+			dice[2] = dice[0];
 			dice[0] = dice[3];
-			dice[3] = dice[1];
-			dice[1] = dice[2];
-			dice[2] = temp;
-			if (arr[x][y]) {
-				dice[0] = arr[x][y];
+			dice[3] = temp;
+			if (arr[x][y] == 0) {
+				arr[x][y] = dice[5];
+			}
+			else {
+				dice[5] = arr[x][y];
 				arr[x][y] = 0;
 			}
-			else
-				arr[x][y] = dice[0];
-			cout << dice[1] << "\n";
-			break;
+			cout << dice[0]<<"\n";
 		}
-		case 2:
-		{
+		else if (move[i] == 2) { //서
 			if (y - 1 < 0)
 				continue;
-			y --;
-			int temp;
-			temp = dice[0];
+			y = y - 1;
+			int temp = dice[5];
+			dice[5] = dice[3];
+			dice[3] = dice[0];
 			dice[0] = dice[2];
-			dice[2] = dice[1];
-			dice[1] = dice[3];
-			dice[3] = temp;
-			if (arr[x][y]) {
-				dice[0] = arr[x][y];
+			dice[2] = temp;
+			if (arr[x][y] == 0) {
+				arr[x][y] = dice[5];
+			}
+			else {
+				dice[5] = arr[x][y];
 				arr[x][y] = 0;
 			}
-			else
-				arr[x][y] = dice[0];
-			cout << dice[1]<<"\n";
-			break;
+			cout << dice[0] << "\n";
 		}
-		case 3:
-		{
+		else if (move[i] == 3) { //북
 			if (x - 1 < 0)
 				continue;
-			x --;
-			int temp;
-			temp = dice[0];
-			dice[0] = dice[5];
+			x = x - 1;
+			int temp = dice[5];
 			dice[5] = dice[1];
-			dice[1] = dice[4];
+			dice[1] = dice[0];
+			dice[0] = dice[4];
 			dice[4] = temp;
-			if (arr[x][y]) {
-				dice[0] = arr[x][y];
+			if (arr[x][y] == 0) {
+				arr[x][y] = dice[5];
+			}
+			else {
+				dice[5] = arr[x][y];
 				arr[x][y] = 0;
 			}
-			else
-				arr[x][y] = dice[0];
-			cout << dice[1] << "\n";
-			break;
+			cout << dice[0] << "\n";
 		}
-		case 4:
-		{
+		else if (move[i] == 4) {
 			if (x + 1 >= n)
 				continue;
-			x ++;
-			int temp;
-			temp = dice[0];
-			dice[0] = dice[4];
-			dice[4] = dice[1];
-			dice[1] = dice[5];
-			dice[5] = temp;
-			if (arr[x][y]) {
-				dice[0] = arr[x][y];
+			x = x + 1;
+			int temp = dice[5];
+			dice[5] = dice[4];
+			dice[4] = dice[0];
+			dice[0] = dice[1];
+			dice[1] = temp;
+			if (arr[x][y] == 0) {
+				arr[x][y] = dice[5];
+			}
+			else {
+				dice[5] = arr[x][y];
 				arr[x][y] = 0;
 			}
-			else
-				arr[x][y] = dice[0];
-			cout << dice[1] << "\n";
-			break;
-		}
-		default:
-			break;
+			cout << dice[0] << "\n";
 		}
 	}
 }
