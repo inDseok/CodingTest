bbec71b0a50d015912825cb5876bc346f93d50a6
전인석 <inseok99@gmail.com>
2025-04-24 16:34:09 +0900

    [level 3] Title: 최적의 행렬 곱셈, Time: 1.40 ms, Memory: 3.95 MB -BaekjoonHub

---
+++ 프로그래머스/3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+long long dp[201][201];
+long d[201];
+int solution(vector<vector<int>> matrix_sizes) {
+    int answer = 0;
+    int m_size=matrix_sizes.size();
+    
+    d[0]=matrix_sizes[0][0];
+    for(int i=0;i<m_size;i++){
+        d[i+1]=matrix_sizes[i][1];
+    }
+    
+    for(int n=0;n<m_size;n++){
+        for(int i=1;i<=m_size-n;i++){
+            int j=i+n;
+            if(i==j){
+                dp[i][j]=0;
+            }
+            else{
+                dp[i][j]=1e9;
+                for(int k=i;k<=j-1;k++){
+                    dp[i][j]=min(dp[i][j],dp[i][k]+dp[k+1][j]+d[i-1]*d[k]*d[j]);
+                }
+            }
+        }
+    }
+    answer=dp[1][m_size];
+    return answer;
+}
